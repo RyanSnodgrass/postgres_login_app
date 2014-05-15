ae67108f5969eb0dac212363f05ff01b5bafc568
RyanSnodgrass <res0428@yahoo.com>
2014-05-15 19:59:15 -0400

    delete and boolean change works. still need to refresh page due to turbolinks error

--- app/assets/javascripts/figure.js
+++ app/assets/javascripts/figure.js
@@ -1,1 +1,57 @@
-figure.js
+$( function() {
+	var ready;
+	ready = function() {
+
+
+	
+	$('.alert').on('click', function() {
+		var row = $(this).parents('tr');
+		var figure_id = $(this).attr('data-figure-id');
+
+
+		$.ajax({
+
+			url: "/figures/" + figure_id,
+			type: 'DELETE',
+
+			success: function(data){
+				if(data == "1") {
+					$(row).hide("slow");
+				}
+				else {
+					$(row).addClass(".failed");
+				}
+			}
+
+
+		});
+	});
+
+	$('.success').on('click', function() {
+		var row = $(this).parents('tr');
+		var figure_id = $(this).attr('data-figure-id');
+		
+		$.ajax({
+			url: "/figures/" + figure_id,
+			type: 'PATCH',
+			data: {
+				figure: {
+					"converted_pastafarian": true
+				}
+			},
+		success: function(data){
+			if(data == "1") {
+				
+			}
+			else {
+				$(row).addClass(".failed");
+			}
+		}
+
+		});
+	});
+	};
+
+	$(document).ready(ready);
+	$(document).on('page:load', ready);
+});
--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,5 +4,50 @@ class FiguresController < ApplicationController
 		@figures = Figure.all
 	end
 
+	def update
+		@figure = Figure.find(params[:id])
+		if @figure.converted_pastafarian = false
+			if @figure.update_attributes(figure_params)
+				respond_to do |format|
+					format.js { render plain: "1"}
+					format.html { redirect_to figures_path}
+				end
+			else
+				respond_to do |format|
+					format.js { render plain: "0"}
+					format.html {redirect_to tasks_path, notice: "Conversion failed" }
+				end
+			end
+		else
+			@figure.converted_pastafarian = false
+			@figure.save
+			respond_to do |format|
+				format.js { render plain: "1"}
+				format.html { redirect_to figures_path}
+			end
+		end
+	end
+
+
+	def destroy
+		@figure = Figure.find(params[:id])
+		if @figure.destroy
+			respond_to do |format|
+				format.js { render plain: "1"}
+				format.html { redirect_to figures_path}
+			end
+		else
+			respond_to do |format|
+				format.js { render plain: "0"}
+				format.html {redirect_to tasks_path, notice: "SHUN failed" }
+			end
+		end
+	end
+
+	private
+	def figure_params
+		params.require(:figure).permit(:converted_pastafarian)
+	end
+
 
 end
